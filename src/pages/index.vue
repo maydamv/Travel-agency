4ad9930799349f312a523949482911021f98edac
maydamv <moralesvieramayda@gmail.com>
2024-08-05 17:02:54 -0400

    import Home in index.vue and update some general styles

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,7 +1,7 @@
 <template>
-  <HelloWorld />
+  <Home />
 </template>
 
 <script setup>
-  //
+    import Home from "@/views/Home.vue"
 </script>
--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -14,6 +14,13 @@ import { createVuetify } from 'vuetify'
 // https://vuetifyjs.com/en/introduction/why-vuetify/#feature-guides
 export default createVuetify({
   theme: {
-    defaultTheme: 'dark',
+    theme: {
+      light: {
+        colors: {
+          primary: "#FEFSF8",
+          secondary: "#SCBBF6",
+        },
+      },
+    },
   },
 })
