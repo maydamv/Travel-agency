ac22c75a30f2af3cb33df5d590e64b00fa25f404
maydamv <moralesvieramayda@gmail.com>
2024-08-05 22:07:50 -0400

    add carousel to our Gallery

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -329,6 +329,22 @@
                     </v-col>
                 </v-row>
             </v-col>
+
+            <v-col cols="12" sm="12">
+                <h4 class="text-center text-indigo-lighten-1">Our Gallery</h4>
+                <h1 class="text-center">Best Traveler Share a photo</h1>
+                <v-sheet class="mx-auto" elevation="0" max-width="1200">
+                    <v-slide-group v-model="model" class="pa-4" center-active show-arrows>
+                        <v-slide-group-item v-for="travel in travels" :key="travel" v-slot="{isSelected, toggle}">
+                            <v-card :color="isSelected ? 'primary' : 'grey-lighten-1'" class="ma-4" height="200" width="130" @click="toggle">
+                                <div class="d-flex fill-height align-center justify-center">
+                                    <v-img :src="travel.image" cover height="100%" width="100%"></v-img>
+                                </div>
+                            </v-card>
+                        </v-slide-group-item>
+                    </v-slide-group>
+                </v-sheet>
+            </v-col>
         </v-row>
     </v-container>
 </v-main>
