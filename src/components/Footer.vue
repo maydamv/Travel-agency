0636e854d6a4cc273673295e15c9402c9b14b694
maydamv <moralesvieramayda@gmail.com>
2024-08-05 22:23:41 -0400

    general structure

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,6 +1,16 @@
 <template>
+    <v-footer color="#FFFFFF" class="d-flex flex-column">
+        <div class="d-flex w-100 px-6 pb-10 pt-8">
 
-    
+        </div>
+        <div class="px-4 py-8 bgTop text-center w-100 text-black">
+            <v-icon class="mr-2" color="black">mdi mdi-copyright</v-icon>
+            <strong>
+                >Copyright {{ new Date().getFullYear() }}. All Rights Reserved by MayMV
+            </strong>
+        </div>
+
+    </v-footer>
 </template>
     
 <script setup>
