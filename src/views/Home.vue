6ca53219d7d99892ca6645fd4fa8e4ddff4e6b19
maydamv <moralesvieramayda@gmail.com>
2024-08-05 21:19:38 -0400

    add presentation

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,10 +1,40 @@
 <template>
-    <Navbar/>
 
-    <v-main>
-    </v-main>
+<Navbar/>
 
-    <Footer/>
+<v-main>
+    <v-container>
+        <v-row>
+            <v-col cols="12">
+                <v-row>
+                    <v-col cols="12" sm="6">
+                        <h1 class="title1">
+                            It's a Big World <br/>
+                            Out There, Go <br/>
+                            Explore
+                        </h1>
+                        <p class="par1">
+                            Conveniently customiz proactiv web service for <br/>
+                                leveraged without continualliery aggregate fricctionle
+                                <br/>ou wellies richard, and very customize continually.
+                        </p>
+                        <v-btn class="text-noe nt-8 bt1" color="indigo-lighten-1" size="x-large" variant="flat" rounded="0">
+                            Get Exploration
+                        </v-btn>
+                        <v-btn class="text-none ml-2 text-indigo-lighten-1 mt-8" size="x-large" variant="flat" rounded="0">
+                            Read More
+                        </v-btn>
+                    </v-col>
+                    <v-col cols="12" sm="6">
+                        <v-img src="/public/2.jpg" class="img1"></v-img>
+                    </v-col>
+                </v-row>
+            </v-col>
+        </v-row>
+    </v-container>
+</v-main>
+
+
 </template>
 
 <script setup>
