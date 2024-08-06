8b5fdac2844fca3ca022649684595568dc3d25e5
maydamv <moralesvieramayda@gmail.com>
2024-08-05 22:19:23 -0400

    declare const hotels in Footer

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -4,6 +4,27 @@
 </template>
     
 <script setup>
+    import {ref} from "vue"
+    const hoteles = ref([
+            {
+                image: "/public/7.jpg"
+            },
+            {
+                image: "/public/16.jpg"
+            },
+            {
+                image: "/public/20.jpg"
+            },
+            {
+                image: "/public/23.jpg"
+            },
+            {
+                image: "/public/17.jpg"
+            },
+            {
+                image: "/public/10.jpg"
+            }
+        ]);
 </script>
     
 <style scoped>
