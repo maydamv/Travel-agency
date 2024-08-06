ed12a77e98c20d6ccfacc7c09e69682d8d6bf00c
maydamv <moralesvieramayda@gmail.com>
2024-08-05 21:53:14 -0400

    add offers for Traveling

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -262,7 +262,26 @@
                 </v-row>
             </v-col>
 
-
+            <v-col cols="12" sm="12">
+                <h4 class="text-center text-indigo-lighten-1">Inventore Varitatis</h4>
+                <h1 class="text-center">Select Offres For Travelings</h1>
+                <v-row justify="center">
+                    <v-col cols="12" sm="10">
+                        <v-row>
+                            <v-col cols="12" sm="2" v-for="(offer,i) in offers" :key="i">
+                                <v-card class="mx-auto px-2 pt-6" height="200px" flat>
+                                    <div align="center" justify="center">
+                                        <v-img :src="offer.image" height="100px" width="100px"></v-img>       
+                                    </div>
+                                    <v-card-title class="text-center">
+                                        <h6>{{ offer.title }}</h6>
+                                    </v-card-title>
+                                </v-card>
+                            </v-col>
+                        </v-row>
+                    </v-col>
+                </v-row>
+            </v-col>
 
         </v-row>
     </v-container>
